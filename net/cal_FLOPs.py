3d4fdff3e2694e65f501af0a7c5f10afee3ac7c8
wangzhilin <1032570740@qq.com>
2020-03-20 17:55:13 +0800

    update

--- net/cal_FLOPs.py
+++ net/cal_FLOPs.py
@@ -2,10 +2,9 @@ from torchstat import stat
 from models import *
 from torchvision import models
 import torch
-# net=UNet64()
-net=models.resnet18()
-print(net)
+net=EUUNet()
+# net=models.resnet18()
 # x=torch.ones([1,3,224,224])
 # net(x)
 # stat(net,(3,224,224))
-# stat(net,(4,1920,1072))
+stat(net,(4,1920,1072))
