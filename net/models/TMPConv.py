40291245f83c2f064fbfcac8e30674f1155d0cd4
wangzhilin <1032570740@qq.com>
2020-03-28 16:10:57 +0800

    update

--- net/models/TMPConv.py
+++ net/models/TMPConv.py
@@ -9,11 +9,13 @@ class FULLCONV(nn.Module):
 		layer.append(nn.Conv2d(3,64,3,1,1))
 		for i in range(1,20):
 			layer.append(nn.Conv2d(64,64,3,3,1))
+			layer.append(nn.ReLU())
 		self.net=nn.Sequential(*layer)
 	def forward(self,x):
 		return self.net(x)
 	
 if __name__ == "__main__":
+	#我操 非常快！！！
 	x=torch.ones([1,3,1024,1024])
 	net=FULLCONV()
 	net(x)
