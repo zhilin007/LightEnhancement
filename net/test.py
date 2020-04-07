e418909d396fafd74e9ec9051d3f228752b40b92
wangzhilin <1032570740@qq.com>
2020-04-07 18:03:44 +0800

    update

--- net/test.py
+++ net/test.py
@@ -1,12 +1,13 @@
 import torch,os,sys,torchvision,tools,glob
 from torchvision import utils
 from train_tensorboard import models_
-from option import opt,cwd
+from option import opt,cwd,model_name
 from data_utils import get_eval_loader
 from PIL import Image
 import torchvision.transforms.transforms as tfs
 def dircheck(path):
 	if not os.path.exists(path):
+		print(path)
 		os.makedirs(path)
 def getNet():
 	net=models_[opt.net].to(opt.device)
@@ -33,7 +34,7 @@ def eval(net,loader):
 				tensorgrid=torch.cat([tensorgrid,pred],0)
 		grid=utils.make_grid(tensorgrid,4)
 		i_gt=tools.get_illumination(targets).item()
-		save_dir=os.path.join(cwd,'grids',opt.pth);dircheck(save_dir)
+		save_dir=os.path.join(cwd,'grids',model_name);dircheck(save_dir)
 		save_dir=os.path.join(save_dir,f'{ind}_in_gt_{i_gt}_0.01_0.1_0.2_0.3_0.4_0.5_0.6_0.7_0.8_0.9_1.png')
 		print(type(grid),grid.shape,ind)
 		utils.save_image(grid,save_dir)
